bfcdffa19078dd8751922bee99039a4bb717f841
Daniel Pietrucha <d.pietrucha@gmi.pl>
2018-05-21 14:56:52 +0200

    Add images bundle feature

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import './style.css';
 import './modules/all';
 import Favicon from './images/favicon.ico';
+import Konrafal from './images/konrafal2.jpg';
 
 function lodashComponent() {
     var element = document.createElement('div');
@@ -12,9 +13,15 @@ function lodashComponent() {
 
     // Add the image to your existing div
     var myFavicon = new Image();
-    myFavicon.src = './dist/' + Favicon;
+    myFavicon.src = '/dist/' + Favicon;
 
     element.appendChild(myFavicon);
+
+    // Add the images as background image
+    var konRafalImage = new Image();
+    konRafalImage.src = Konrafal;
+
+    // element.appendChild(konRafalImage);
 
     return element;
 }
