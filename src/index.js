7ea116fef17da77dcaac77db984325cef5eed6ef
Daniel Pietrucha <d.pietrucha@gmi.pl>
2018-05-21 14:36:46 +0200

    Add images feature

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import _ from 'lodash';
 import './style.css';
 import './modules/all';
+import Favicon from './images/favicon.ico';
 
 function lodashComponent() {
     var element = document.createElement('div');
 
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack', 'from', 'index.js'], ' ');
     element.classList.add('hello');
+
+    // Add the image to your existing div
+    var myFavicon = new Image();
+    myFavicon.src = './dist/' + Favicon;
+
+    element.appendChild(myFavicon);
 
     return element;
 }
 
 // lodash component is for learning cases only
-// commented now
-// document.body.appendChild(lodashComponent());
+document.body.appendChild(lodashComponent());
--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,19 @@ module.exports = {
     },
     module: {
          rules: [
-           {
-             test: /\.css$/,
-             use: [
-               'style-loader',
-               'css-loader'
-             ]
-       }
-     ]
-   }
+             {
+                 test: /\.css$/,
+                 use: [
+                   'style-loader',
+                   'css-loader'
+                 ]
+             },
+             {
+                 test: /\.(png|svg|jpg|gif|ico)$/,
+                 use: [
+                     'file-loader'
+                 ]
+             }
+         ]
+    }
 };
